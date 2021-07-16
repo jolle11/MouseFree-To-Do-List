835ccbe17cd096cfcf143e98aa91833167fc52f6
Jordi Ollé Ballesté <jordiolleballeste@Jordis-MBP.lan>
2021-07-16 17:16:50 +0200

    Modified functions for A, C, U, D. Added function to focus and unfocus to the input.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // SELECTORS
-
 const todoInput = document.getElementById('todo-input');
 const todos = document.querySelector('.todos');
 
@@ -15,39 +14,55 @@ const dLong = document.querySelector('#d-long');
 
 // EVENT LISTENERS
 
-// This next event listener has the function to change the display of each titles
+// This next event listener has the direct function to change the display of each titles
 // and to show the correct to do's. Example: Key C shows title completed
 // and short title for All, Uncompleted and Deleted and only the completed to do's.
 document.addEventListener('keydown', (e) => {
-    if ((e.key === 'a' && todoInput.value === '') || (e.code === 'KeyA' && todoInput.value === '')) {
-        showAll(e);
-    } else if ((e.key === 'c' && todoInput.value === '') || (e.code === 'KeyC' && todoInput.value === '')) {
-        showCompleted(e);
-    } else if ((e.key === 'u' && todoInput.value === '') || (e.code === 'KeyU' && todoInput.value === '')) {
-        showUncompleted(e);
-    } else if ((e.key === 'd' && todoInput.value === '') || (e.code === 'KeyD' && todoInput.value === '')) {
-        showDeleted(e);
+    if (e.key === 'Enter' || e.code === 'Enter') {
+        focusInput(e);
+        todoInput.value = ' ';
+    } else if (e.key === 'Escape' || e.code === 'Escape') {
+        unfocusInput(e);
+        todoInput.value = '';
+    } else if (e.key === 'a' || e.code === 'KeyA') {
+        if (todoInput.value === '') {
+            showAll(e);
+        } else {
+            return;
+        }
+    } else if (e.key === 'c' || e.code === 'KeyC') {
+        if (todoInput.value === '') {
+            showCompleted(e);
+        } else {
+            return;
+        }
+    } else if (e.key === 'u' || e.code === 'KeyU') {
+        if (todoInput.value === '') {
+            showUncompleted(e);
+        } else {
+            return;
+        }
+    } else if (e.key === 'd' || e.code === 'KeyD') {
+        if (todoInput.value === '') {
+            showDeleted(e);
+        } else {
+            return;
+        }
     }
 });
 
-// This event listener adds the to do to the list.
+// This event listener calls the function addTodo, which adds the to do to the list.
 todoInput.addEventListener('keydown', (e) => {
     if (e.key === 'Enter' || e.code === 'Enter') {
-        //Checks whether the pressed key is "Enter"
         addTodo(e);
     }
 });
 
 // FUNCTIONS
 
-// With this onload function we make sure every time the page is refreshed all the todos are shown
+// With this onload function we make sure every time the page is refreshed all the uncompleted todos are shown.
 window.onload = function () {
-    aLong.classList.add('hidden');
-    cLong.classList.add('hidden');
-    uShort.classList.add('hidden');
-    dLong.classList.add('hidden');
-    inputSelect = document.querySelector('#todo-input');
-    inputSelect.value = '';
+    showUncompleted();
 };
 
 function showAll() {
@@ -91,10 +106,20 @@ function showDeleted() {
     dShort.classList.add('hidden');
 }
 
+// Function to focus the input when clicking ENTER
+function focusInput() {
+    document.getElementById('todo-input').focus();
+}
+
+// Function to unfocus the input when clicking ESCAPE
+function unfocusInput() {
+    document.getElementById('todo-input').blur();
+}
+
 // The next function adds a to do to the list
 function addTodo() {
     inputSelect = document.querySelector('#todo-input');
     inputValue = inputSelect.value;
-    console.log(inputValue);
+    console.log(inputValue.trimStart());
     console.log('Works!');
 }
